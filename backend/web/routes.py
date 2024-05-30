d962ceb75c23efc15190698fcbfb4dd43545ec4a
Nelit <nelit2714@gmail.com>
2024-05-31 00:56:15 +0200

    добавлен гет запрос на получение карт, изменена логика возврата ответа с темами, добавление новых вопросов

--- backend/web/routes.py
+++ backend/web/routes.py
@@ -1,3 +1,4 @@
+from bson import ObjectId
 from fastapi import Request, UploadFile, File, Form
 from fastapi.responses import JSONResponse
 
@@ -5,14 +6,21 @@ from web import app, mongo
 from web.functions import get_file, save_file
 
 
-
 @app.get("/api/get_themes/")
-async def get_cards() -> JSONResponse:
-    themes_cursor = mongo["themes"].find({})
-    themes = await themes_cursor.to_list(length=None)
+async def get_themes() -> JSONResponse:
+    themes = await mongo["themes"].find({}).to_list(length=None)
     for theme in themes:
         theme["_id"] = str(theme["_id"])
     return JSONResponse({"themes": themes})
+
+
+@app.get("/api/get_cards/{theme_id}")
+async def get_cards(theme_id: str) -> JSONResponse:
+    print(theme_id)
+    theme = await mongo["themes"].find_one({"_id": ObjectId(theme_id)})
+    cards = theme.get("cards", [])
+    print(cards)
+    return JSONResponse({"cards": cards})
 
 
 @app.post("/api/add_theme/")
@@ -22,8 +30,21 @@ async def add_theme(name: str = Form(...), description: str = Form(...), picture
         "name": name,
         "description": description,
         "picture": picture_filename,
+        "cards": [],
         "completed": 0,
         "views": 0
     })
+    return JSONResponse({"status": True})
 
-    return {"message": "Theme added successfully"}
+
+@app.post("/api/add_question/")
+async def add_question(theme_id: str = Form(...), question: str = Form(...), answer: str = Form(...)):
+    theme = await mongo["themes"].find_one({"_id": ObjectId(theme_id)})
+    await mongo["themes"].update_one(
+        {"_id": theme.get("_id")},
+        {"$addToSet": {"cards": {
+            "question": str(question),
+            "answer": str(answer)
+        }}}
+    )
+    return JSONResponse({"status": True})
